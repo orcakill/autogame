fb11210c313fb25205bc49fb51b5861162b010a4
orcakill <auto-commit@example.com>
2026-09-02 20:10:37 +0800

    fix(调整python代码)：调整 model/enum.py

--- src/test/test_onmyoji_service.py
+++ src/test/test_onmyoji_service.py
@@ -39,7 +39,7 @@ class TestOnmyojiService(TestCase):
         4 云手机-0021
         :return:
         """
-        TestOnmyojiService.test_project('1', "0", "登录")
+        TestOnmyojiService.test_project('1', "5", "登录")
 
     def test_initialization1(self):
         """
